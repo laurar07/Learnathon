b2cbd86d7dcbc988168f8f19e2f00bda426a19de
Laura Rodriguez Mosquera <lauraro@Lauras-MacBook-Pro.local>
2017-11-27 22:04:00 -0800

    Added screenshots and fixed styling

--- components/AddCard.js
+++ components/AddCard.js
@@ -6,17 +6,17 @@ import { addCard } from '../actions'
 import { white, purple, gray } from '../utils/colors'
 
 function SubmitBtn ({ onPress }) {
-    return (
-        <TouchableOpacity
-            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
-            onPress={onPress}>
-                <Text style={styles.submitBtnText}>Add</Text>
-        </TouchableOpacity>
-    )
+	return (
+		<TouchableOpacity
+			style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
+			onPress={onPress}>
+			<Text style={styles.submitBtnText}>Add</Text>
+		</TouchableOpacity>
+	)
 }
 
 class AddCard extends Component {
-    state = {
+	state = {
         question: '',
         answer: ''
     }
@@ -74,86 +74,85 @@ class AddCard extends Component {
             )
         }
     }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View>
-                    <Text style={styles.deckName}>
+	render() {
+		return (
+			<View style={styles.container}>
+				<View>
+					<Text style={styles.deckName}>
                         Enter the question to add
-                    </Text>
-                    <TextInput
-                        style={{ height: 40, borderColor: gray, borderWidth: 1 }}
-                        onChangeText={(text) => this.setState({ question: text })}
-                        value={this.state.question}
-                    />
-                    <View style={styles.space}>
-                    </View>
-                    <Text style={styles.deckName}>
+					</Text>
+					<TextInput
+						style={{ height: 40, borderColor: gray, borderWidth: 1 }}
+						onChangeText={(text) => this.setState({ question: text })}
+						value={this.state.question}
+					/>
+					<View style={styles.space}>
+					</View>
+					<Text style={styles.deckName}>
                         Enter the correct answer
-                    </Text>
-                    <TextInput
-                        style={{ height: 40, borderColor: gray, borderWidth: 1 }}
-                        onChangeText={(text) => this.setState({ answer: text })}
-                        value={this.state.answer}
-                    />
-                </View>
-                <View style={[{flex: 1}]}>
-                </View>
-                <SubmitBtn onPress={this.submit}/>
-            </View>
-        )
-    }
+					</Text>
+					<TextInput
+						style={{ height: 40, borderColor: gray, borderWidth: 1 }}
+						onChangeText={(text) => this.setState({ answer: text })}
+						value={this.state.answer}
+					/>
+				</View>
+				<View style={[{flex: 1}]}>
+				</View>
+				<SubmitBtn onPress={this.submit}/>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: white
-    },
-    space: {
-        height: 80
-    },
-    iosSubmitBtn: {
-        backgroundColor: purple,
-        padding: 10,
-        borderRadius: 7,
-        height: 45,
-        marginLeft: 40,
-        marginRight: 40
-    },
-    androidSubmitBtn: {
-        backgroundColor: purple,
-        padding: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        height: 45,
-        borderRadius: 2,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    submitBtnText: {
-        color: white,
-        fontSize: 22,
-        textAlign: 'center'
-    },
-    deckName: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 20,
-        paddingTop: 20,
-        paddingBottom: 20
-    }
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: white
+	},
+	space: {
+		height: 80
+	},
+	iosSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		marginLeft: 40,
+		marginRight: 40
+	},
+	androidSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		paddingLeft: 30,
+		paddingRight: 30,
+		height: 45,
+		borderRadius: 2,
+		alignSelf: 'center',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	submitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center'
+	},
+	deckName: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: 20,
+		paddingTop: 20,
+		paddingBottom: 20
+	}
 })
 
 function mapStateToProps(state, { navigation }) {
-    const { deck } = navigation.state.params
-    const currDeck = state['decks'].filter((d) => d.name === deck.name)[0]
-    return {
-        deck: currDeck
-    }
+	const { deck } = navigation.state.params
+	const currDeck = state['decks'].filter((d) => d.name === deck.name)[0]
+	return {
+		deck: currDeck
+	}
 }
 
 export default connect(mapStateToProps)(AddCard)
--- components/Quiz.js
+++ components/Quiz.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, Platform, StyleSheet, Animated } from 'react-native'
 import { 
-    clearLocalNotification, 
-    setLocalNotification 
+	clearLocalNotification, 
+	setLocalNotification 
 } from '../utils/helpers'
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
@@ -11,50 +11,50 @@ import { NavigationActions } from 'react-navigation'
 import Results from './Results'
 
 function TopBtn ({ onPress, quizComplete }) {
-    const btnText = quizComplete ? 'Restart Quiz' : 'Correct'
-    return (
-        <TouchableOpacity
-            style={[Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn,
-                !quizComplete 
-                ? { backgroundColor: blue }
-                : { backgroundColor: purple, borderWidth: 2, borderColor: black }]}
-            onPress={onPress}>
-                <Text style={
-                    !quizComplete
-                    ? styles.btnText
-                    : [styles.submitBtnText, { color: white }]}>{btnText}</Text>
-        </TouchableOpacity>
-    )
+	const btnText = quizComplete ? 'Restart Quiz' : 'Correct'
+	return (
+		<TouchableOpacity
+			style={[Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn,
+				!quizComplete 
+					? { backgroundColor: blue }
+					: { backgroundColor: purple, borderWidth: 2, borderColor: black }]}
+			onPress={onPress}>
+			<Text style={
+				!quizComplete
+					? styles.btnText
+					: [styles.submitBtnText, { color: white }]}>{btnText}</Text>
+		</TouchableOpacity>
+	)
 }
 
 function BottomBtn ({ onPress, quizComplete }) {
-    const btnText = quizComplete ? 'Back to Deck' : 'Incorrect'
-    return (
-        <TouchableOpacity
-            style={[Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn, 
-            !quizComplete 
-            ? { backgroundColor: red }
-            : { backgroundColor: white, borderWidth: 2, borderColor: black }]}
-            onPress={onPress}>
-                <Text style={
-                    !quizComplete
-                    ? styles.btnText
-                    : [styles.submitBtnText, { color: black }]}>{btnText}</Text>
-        </TouchableOpacity>
-    )
+	const btnText = quizComplete ? 'Back to Deck' : 'Incorrect'
+	return (
+		<TouchableOpacity
+			style={[Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn, 
+				!quizComplete 
+					? { backgroundColor: red }
+					: { backgroundColor: white, borderWidth: 2, borderColor: black }]}
+			onPress={onPress}>
+			<Text style={
+				!quizComplete
+					? styles.btnText
+					: [styles.submitBtnText, { color: black }]}>{btnText}</Text>
+		</TouchableOpacity>
+	)
 }
 
 function incrementScore() {
-    return (previousState, currentProps) => {
-        return { 
-            ...previousState, 
-            score: previousState.score + 1 
-        };
-    };
+	return (previousState, currentProps) => {
+		return { 
+			...previousState, 
+			score: previousState.score + 1 
+		};
+	};
 }
 
 class Quiz extends Component {
-    state = {
+	state = {
         index: 0,
         score: 0,
         flipped: false,
@@ -154,128 +154,128 @@ class Quiz extends Component {
         const { index } = this.state
         return (index >= deck.cards.length); 
     }
-    render() {
-        const { deck } = this.props
-        const { 
-            index, 
-            score, 
-            flipped,
-            animatedValue
-        } = this.state
-        const rotateY = animatedValue.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['0deg', '360deg']
-        });
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.right} onPress={this.toHome}>
-                    {Platform.OS === 'ios' 
-                    ?   <Ionicons name='ios-close' size={50} color={black}/>
-                    :   <MaterialIcons name='close' size={50} color={black}/>}
-                </TouchableOpacity>
-                {!this.quizComplete() && (
-                    <View style={styles.center}>
-                        <Text style={[styles.numberQuestionText, {fontSize: 20}]}>
+	render() {
+		const { deck } = this.props
+		const { 
+			index, 
+			score, 
+			flipped,
+			animatedValue
+		} = this.state
+		const rotateY = animatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['0deg', '360deg']
+		})
+		return (
+			<View style={styles.container}>
+				<TouchableOpacity style={styles.right} onPress={this.toHome}>
+					{Platform.OS === 'ios' 
+						?   <Ionicons name='ios-close' size={50} color={black}/>
+						:   <MaterialIcons name='close' size={50} color={black}/>}
+				</TouchableOpacity>
+				{!this.quizComplete() && (
+					<View style={styles.center}>
+						<Text style={[styles.numberQuestionText, {fontSize: 20}]}>
                             Question {index + 1} of {deck.cards.length}
-                        </Text>
-                        <Text style={[styles.questionAnswerText, {fontSize: 40}]}>
-                            {deck.cards[index].question}
-                        </Text>
-                        <TouchableOpacity onPress={this.flip}>
-                            <Animated.View style={{transform: [{rotateY}]}}>
-                                {flipped && this.renderBack()}
-                                {!flipped && this.renderFront()}
-                            </Animated.View>
-                        </TouchableOpacity>
-                    </View>
-                )}
-                {this.quizComplete() && (
-                    <Results
-                        deck={deck}
-                        score={score} />
-                )}
-                   <View>
-                        <TopBtn onPress={!this.quizComplete() 
-                            ? this.correct 
-                            : this.restartQuiz} 
-                                quizComplete={this.quizComplete()} />
-                        <View style={styles.space}>
-                        </View>
-                        <BottomBtn onPress={!this.quizComplete() 
-                            ? this.incorrect
-                            : this.navigateToDeck}
-                                quizComplete={this.quizComplete()} />
-                    </View>
-            </View>
-        )
-    }
+						</Text>
+						<Text style={[styles.questionAnswerText, {fontSize: 40}]}>
+							{deck.cards[index].question}
+						</Text>
+						<TouchableOpacity onPress={this.flip}>
+							<Animated.View style={{transform: [{rotateY}]}}>
+								{flipped && this.renderBack()}
+								{!flipped && this.renderFront()}
+							</Animated.View>
+						</TouchableOpacity>
+					</View>
+				)}
+				{this.quizComplete() && (
+					<Results
+						deck={deck}
+						score={score} />
+				)}
+				<View>
+					<TopBtn onPress={!this.quizComplete() 
+						? this.correct 
+						: this.restartQuiz} 
+					quizComplete={this.quizComplete()} />
+					<View style={styles.space}>
+					</View>
+					<BottomBtn onPress={!this.quizComplete() 
+						? this.incorrect
+						: this.navigateToDeck}
+					quizComplete={this.quizComplete()} />
+				</View>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: white
-    },
-    iosBtn: {
-        padding: 10,
-        borderRadius: 7,
-        height: 45,
-        marginLeft: 40,
-        marginRight: 40
-    },
-    androidBtn: {
-        padding: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        height: 45,
-        borderRadius: 2,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    btnText: {
-        color: white,
-        fontSize: 22,
-        textAlign: 'center'
-    },
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 30,
-        marginRight: 30,
-        paddingBottom: 30
-    },
-    right: {
-        justifyContent: 'center',
-        alignSelf: 'flex-end'
-    },
-    space: {
-        height: 20
-    },
-    questionAnswerText: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        paddingTop: 20,
-        paddingBottom: 20
-    },
-    numberQuestionText: {
-        textAlign: 'center',
-        fontWeight: 'bold'
-    },
-    submitBtnText: {
-        color: black,
-        fontSize: 22,
-        textAlign: 'center'
-    }
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: white
+	},
+	iosBtn: {
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		marginLeft: 40,
+		marginRight: 40
+	},
+	androidBtn: {
+		padding: 10,
+		paddingLeft: 30,
+		paddingRight: 30,
+		height: 45,
+		borderRadius: 2,
+		alignSelf: 'center',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	btnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center'
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 30,
+		marginRight: 30,
+		paddingBottom: 30
+	},
+	right: {
+		justifyContent: 'center',
+		alignSelf: 'flex-end'
+	},
+	space: {
+		height: 20
+	},
+	questionAnswerText: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+		paddingTop: 20,
+		paddingBottom: 20
+	},
+	numberQuestionText: {
+		textAlign: 'center',
+		fontWeight: 'bold'
+	},
+	submitBtnText: {
+		color: black,
+		fontSize: 22,
+		textAlign: 'center'
+	}
 })
 
 function mapStateToProps(state, { navigation }) {
-    const { deck } = navigation.state.params
-    return {
-        deck
-    }
+	const { deck } = navigation.state.params
+	return {
+		deck
+	}
 }
 
 export default connect(mapStateToProps)(Quiz)
